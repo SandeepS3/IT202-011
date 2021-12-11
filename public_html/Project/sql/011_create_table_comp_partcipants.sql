0664dfd8c53353b9956a8dd06cd9f900e4b11e36
Sandeep Singh <sandeeps3@Sandeeps-MacBook-Pro.local>
2021-12-10 20:51:03 -0500

    Can create competition

---
+++ public_html/Project/sql/011_create_table_comp_partcipants.sql
@@ -0,0 +1,9 @@
+CREATE TABLE CompetitionParticipants (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  comp_id int,
+  user_id int,
+  created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  unique key(user_id, comp_id),
+  FOREIGN KEY (user_id) REFERENCES Users(id)
+)
